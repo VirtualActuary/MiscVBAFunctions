c045fab25cc4ca28f84b837f9e0ce4b053aa0a15
heetbeet <sfstreicher@gmail.com>
2022-06-07 14:46:21 +0200

    Drop support for *Lib version of the code

--- scripts/compile.py
+++ scripts/compile.py
@@ -4,24 +4,13 @@ from pathlib import Path
 from zebra_vba_packager import compile_xl, is_locked, runmacro_xl, backup_last_50_paths
 import locate
 
-with locate.prepend_sys_path("."):
-    from util import app_name, get_app_clean
-
+app_name = "MiscVBAFunctions"
 app_xl = locate.this_dir().joinpath(f"../{app_name}.xlsb")
-app_lib_xl = locate.this_dir().joinpath(f"../{app_name}Lib.xlsb")
 app_dir = Path(str(app_xl)[:-5])
 
-for i in [app_xl, app_lib_xl]:
-    if i.exists():
-        backup_last_50_paths(Path(tempfile.gettempdir(), f"{app_name}-compile-backups"), i)
+if app_xl.exists():
+    backup_last_50_paths(Path(tempfile.gettempdir(), f"{app_name}-compile-backups"), app_xl)
 
 # Compile and run early bindings
 compile_xl(app_dir, app_xl)
-with get_app_clean() as tmp:
-    for i in app_dir.rglob("*.xlsx"):
-        shutil.copy2(i, Path(tmp, i.name))
-        
-    compile_xl(tmp, app_lib_xl)
-
 runmacro_xl(app_xl)
-runmacro_xl(app_lib_xl)
