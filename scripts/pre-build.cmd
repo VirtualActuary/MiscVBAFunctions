d72b24a33219c4a1a1bb79f4f1ffc64cfc1f2d0d
Malan <malankriel@gmail.com>
2022-06-09 11:19:03 +0200

    add downloadable template to documentation

--- scripts/pre-build.cmd
+++ scripts/pre-build.cmd
@@ -1,12 +1,13 @@
 @echo off
 
+:: TODO: add zebra-vba-packager in requirements.txt
+::pip3 install -r "%~dp0\requirements.txt" ' 
+python "%~dp0\compile.py"
+
 pip3 install -r "%~dp0\..\docs\requirements.txt"
 
 pushd "%~dp0\..\docs"
     mkdocs build -f mkdocs.yml
 popd
-
-python "%~dp0\compile.py"
-
 
 if /i "%comspec% /c ``%~0` `" equ "%cmdcmdline:"=`%" pause
