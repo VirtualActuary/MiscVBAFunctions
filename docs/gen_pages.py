5a18650a7bdb4b9453079d171e5e21762b6e872b
dolf <rudolfbyker@gmail.com>
2022-09-05 10:16:02 +0200

    Refactor gen_pages.py

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -6,21 +6,23 @@ def main():
     repo_dir = Path("..")
     nav = mkdocs_gen_files.Nav()
 
-    # Inject the repo's README.md file into the documentation as the starting page.
-    nav["Introduction"] = "index.md"
+    # Copy the template so that we can link to it from the docs.
     with mkdocs_gen_files.open("MiscVbaTemplate.xlsb", "wb") as dst:
-        with open('../MiscVbaTemplate.xlsb', "rb") as s:
+        with open("../MiscVbaTemplate.xlsb", "rb") as s:
             dst.write(s.read())
-    
+
+    nav["Introduction"] = "index.md"
     with mkdocs_gen_files.open("index.md", "w") as dst:
-        with repo_dir.joinpath("README.md").open("r") as src:
-            dst.write(
-                """---
+        dst.write(
+            """---
 title: MiscVba
 ---
 Click [here](MiscVbaTemplate.xlsb) to download the template.
 """
-            )
+        )
+
+        # Inject the repo's README.md file into the documentation as the starting page.
+        with repo_dir.joinpath("README.md").open("r") as src:
             dst.write(src.read())
 
     # Generate docs from source code and inject navigation for generated files.
@@ -28,7 +30,11 @@ Click [here](MiscVbaTemplate.xlsb) to download the template.
     for src_dir in [
         repo_dir.joinpath("MiscVBAFunctions"),
     ]:
-        for src_path in [i for i in sorted(src_dir.rglob("*.bas")) if not i.name.lower().startswith('test__')]:
+        for src_path in [
+            i
+            for i in sorted(src_dir.rglob("*.bas"))
+            if not i.name.lower().startswith("test__")
+        ]:
             doc_path = src_path.relative_to(repo_dir).with_suffix(".md")
 
             nav[src_path.relative_to(repo_dir).parts] = doc_path.as_posix()
