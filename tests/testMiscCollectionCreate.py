70f44429092762ba498b562979190fc945942a54
Bart-Jan Hulsman <hulsmanbj@gmail.com>
2023-01-31 10:36:59 +0200

    Added MiscCollectionCreate tests (excluding zip)

---
+++ tests/testMiscCollectionCreate.py
@@ -0,0 +1,21 @@
+import unittest
+from .util import functions_book
+
+
+class TestMin(unittest.TestCase):
+    def test_1(self) -> None:
+        with functions_book() as book:
+            with self.subTest("Col"):
+                func_col = book.macro("MiscCollectionCreate.Col")
+                func_Col_to_arr = book.macro("MiscCollection.CollectionToArray")
+                col = func_col(1, 4, 5)
+                arr = func_Col_to_arr(col)
+                self.assertEqual(1, arr[0])
+                self.assertEqual(4, arr[1])
+                self.assertEqual(5, arr[2])
+
+
+if __name__ == "__main__":
+    unittest.main(
+        failfast=True,
+    )
