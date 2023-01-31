51c99579af3bba242ed5dc8c13e1a2c57aa6d242
Bart-Jan Hulsman <hulsmanbj@gmail.com>
2023-01-31 13:43:58 +0200

    Added testMiscDictsToTable tests (excluding failing tests)

--- tests/testMiscDictsToTable.py
+++ tests/testMiscDictsToTable.py
@@ -10,29 +10,51 @@ class TestDictsToTable(unittest.TestCase):
     def test_1(self) -> None:
         book: Book
         with functions_book() as book:
-            func_dicts_to_table = book.macro("MiscDictsToTable.DictsToTable")
-            func_col = book.macro("MiscCollectionCreate.Col")
+            with self.subTest("DictsToTable_1"):
+                func_dicts_to_table = book.macro("MiscDictsToTable.DictsToTable")
+                func_col = book.macro("MiscCollectionCreate.Col")
 
-            func_dicts_to_table(
-                func_col(
-                    vba_dict({"a": 1, "b": 2}),
-                    vba_dict({"a": 3, "b": 4}),
-                    vba_dict({"a": 5, "b": 6}),
-                ),
-                book.sheets["Sheet1"].range("A1"),
-                "Table1",
-                False,
-            )
+                d1 = vba_dict({"a": 1, "b": 2})
+                func_dicts_to_table(
+                    func_col(d1, d1),
+                    book.sheets["Sheet1"].range("A1"),
+                    "Table1",
+                    False,
+                )
 
-            table = Table.get_from_book(book, "Table1")
+                table = Table.get_from_book(book, "Table1")
 
-            self.assertEqual("$A$1:$B$4", table.range.address)
-            self.assertEqual("Table1", table.name)
-            self.assertEqual("Sheet1", table.sheet.name)
-            self.assertEqual(
-                [["a", "b"], [1, 2], [3, 4], [5, 6]],
-                table.range.value,
-            )
+                self.assertEqual("$A$1:$B$3", table.range.address)
+                self.assertEqual("Table1", table.name)
+                self.assertEqual("Sheet1", table.sheet.name)
+                self.assertEqual(
+                    [["a", "b"], [1, 2], [1, 2]],
+                    table.range.value,
+                )
+
+            with self.subTest("DictsToTable_2"):
+                func_dicts_to_table = book.macro("MiscDictsToTable.DictsToTable")
+                func_col = book.macro("MiscCollectionCreate.Col")
+
+                func_dicts_to_table(
+                    func_col(
+                        vba_dict({"a": 1, "b": 2, "c": 3}),
+                        vba_dict({"a": 10, "c": 20, "b": 30}),
+                    ),
+                    book.sheets["Sheet1"].range("D1"),
+                    "Table2",
+                    False,
+                )
+
+                table = Table.get_from_book(book, "Table2")
+
+                self.assertEqual("$D$1:$F$3", table.range.address)
+                self.assertEqual("Table2", table.name)
+                self.assertEqual("Sheet1", table.sheet.name)
+                self.assertEqual(
+                    [["a", "b", "c"], [1, 2, 3], [10, 30, 20]],
+                    table.range.value,
+                )
 
 
 if __name__ == "__main__":
