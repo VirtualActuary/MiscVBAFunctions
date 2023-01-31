1bb8968fe5a55f056f13391a2e6515641755e461
Bart-Jan Hulsman <hulsmanbj@gmail.com>
2023-01-31 14:28:31 +0200

    Added testMiscEnsureDictUtil tests (excluding failing tests)

---
+++ tests/testMiscEnsureDictIUtil.py
@@ -0,0 +1,25 @@
+import unittest
+from xlwings import Book
+from .util import functions_book, vba_dict
+
+
+class TestDictsToTable(unittest.TestCase):
+    def test_1(self) -> None:
+        book: Book
+        with functions_book() as book:
+            with self.subTest("EnsureDictI"):
+                func_EnsureDictI = book.macro("MiscEnsureDictIUtil.EnsureDictI")
+                d1 = vba_dict({"a": 1})
+                d2 = func_EnsureDictI(d1)
+
+                self.assertTrue(d1.exists("a"))
+                self.assertFalse(d1.exists("A"))
+
+                self.assertTrue(d2.exists("a"))
+                self.assertTrue(d2.exists("A"))
+
+
+if __name__ == "__main__":
+    unittest.main(
+        failfast=True,
+    )
