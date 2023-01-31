07e10e1f8490a8e0c1e9a5416ccdacf98a0b9f38
Bart-Jan Hulsman <hulsmanbj@gmail.com>
2023-01-31 15:26:40 +0200

    Added testMiscGetUniqueItems tests

---
+++ tests/testMiscGetUniqueItems.py
@@ -0,0 +1,26 @@
+import unittest
+from xlwings import Book
+from .util import functions_book
+
+
+class TestDictsToTable(unittest.TestCase):
+    def test_1(self) -> None:
+        book: Book
+        with functions_book() as book:
+            with self.subTest("GetUniqueItems"):
+                func_GetUniqueItems = book.macro("MiscGetUniqueItems.GetUniqueItems")
+
+                self.assertEqual(3, len(func_GetUniqueItems(["a", "b", "c", "b"])))
+                self.assertEqual(4, len(func_GetUniqueItems(["a", "b", "c", "B"])))
+                self.assertEqual(
+                    3, len(func_GetUniqueItems(["a", "b", "c", "B"], False))
+                )
+
+                self.assertEqual(3, len(func_GetUniqueItems([1, 2, 3, 2])))
+                self.assertEqual(2, len(func_GetUniqueItems([1, 1, "a", "a"])))
+
+
+if __name__ == "__main__":
+    unittest.main(
+        failfast=True,
+    )
