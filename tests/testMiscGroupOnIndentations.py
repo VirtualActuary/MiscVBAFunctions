cf6957511f9fe8fbd64368a86da9ee5819c8f2fc
Bart-Jan Hulsman <91005483+BartJan142@users.noreply.github.com>
2023-02-08 10:54:12 +0200

    Added MiscGroupOnIndentations tests

---
+++ tests/testMiscGroupOnIndentations.py
@@ -0,0 +1,49 @@
+import unittest
+from xlwings import Book
+from locate import prepend_sys_path, this_dir
+from aa_py_xl import excel_app
+
+with prepend_sys_path():
+    from util import functions_book, extra_book
+
+
+class TestDictsToTable(unittest.TestCase):
+    def test_1(self) -> None:
+        with excel_app(True, True) as app:
+            book: Book
+            with functions_book(app) as book:
+                repo_path = this_dir().parent
+                repo_path = repo_path.joinpath(
+                    r"test_data\MiscGroupOnIndentations\MiscGroupOnIndentations.xlsx"
+                )
+                book_extra: Book
+                with extra_book(app, repo_path) as book_extra:
+                    with self.subTest("TestGroupOnIndentationsRows"):
+                        func = book.macro(
+                            "Test__Helper_MiscGroupOnIndent.TestGroupOnIndentationsRows"
+                        )
+                        self.assertTrue(func(book_extra))
+
+                    with self.subTest("TestGroupOnIndentationsColumns"):
+                        func = book.macro(
+                            "Test__Helper_MiscGroupOnIndent.TestGroupOnIndentationsColumns"
+                        )
+                        self.assertTrue(func(book_extra))
+
+                    with self.subTest("TestGroupOnIndentationsColumns"):
+                        func = book.macro(
+                            "Test__Helper_MiscGroupOnIndent.TestUnGroupOnIndentationsRow"
+                        )
+                        self.assertTrue(func(book_extra))
+
+                    with self.subTest("TestGroupOnIndentationsColumns"):
+                        func = book.macro(
+                            "Test__Helper_MiscGroupOnIndent.TestUnGroupOnIndentationsCol"
+                        )
+                        self.assertTrue(func(book_extra))
+
+
+if __name__ == "__main__":
+    unittest.main(
+        failfast=True,
+    )
