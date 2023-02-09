186710648ff653cb1c85b7d9c3b8dcd100c303cd
Bart-Jan Hulsman <91005483+BartJan142@users.noreply.github.com>
2023-02-09 08:58:17 +0200

    new test Python

--- tests/testMiscNewKeys.py
+++ tests/testMiscNewKeys.py
@@ -10,11 +10,13 @@ class MiscHasKey(unittest.TestCase):
     def test_1(self) -> None:
         book: Book
         with functions_book() as book:
-            with self.subTest("Test_GetNewKey"):
-                func = book.macro(
-                    "Test__Helper_MiscNewKeys.Test_GetNewKey"
-                )
-                self.assertTrue(func())
+            with self.subTest("Test_GetNewKey1"):
+                func_GetNewKey = book.macro("Test__Helper_MiscNewKeys.Test_GetNewKey1")
+                self.assertTrue(func_GetNewKey())
+
+            with self.subTest("Test_GetNewKey2"):
+                func_GetNewKey = book.macro("Test__Helper_MiscNewKeys.Test_GetNewKey2")
+                self.assertTrue(func_GetNewKey())
 
 
 if __name__ == "__main__":
