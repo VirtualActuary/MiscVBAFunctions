d6cc24b502139b7aa827942cc17228b008a17a20
Bart-Jan Hulsman <91005483+BartJan142@users.noreply.github.com>
2023-02-09 11:58:22 +0200

    Added MiscTables VBA tests

---
+++ MiscVBAFunctions/Modules/Test__Helper_MiscTables.bas
@@ -0,0 +1,153 @@
+Attribute VB_Name = "Test__Helper_MiscTables"
+Option Explicit
+
+Function TestHasLO_and_GetLO(WB As Workbook)
+    Dim Pass As Boolean
+    Pass = True
+    
+    Pass = True = HasLO("Table1", WB) = Pass ' Correct case
+    Pass = True = HasLO("taBLe1", WB) = Pass ' Should work without correct case
+
+    Pass = "Table1" = GetLO("taBLe1", WB).Name = Pass ' Should get the correct name even with a different casing
+
+    TestHasLO_and_GetLO = Pass
+End Function
+
+
+Function TestTableRange(WB As Workbook)
+    Dim TableR As Range
+    Dim Pass As Boolean
+    Pass = True
+
+    Set TableR = TableRange("table1", WB)
+    Pass = "Column1" = TableR.Cells(1).Value = Pass
+
+    Set TableR = TableRange("NamedRange1", WB)
+    Pass = "Column1" = TableR.Cells(1).Value = Pass
+
+    Set TableR = TableRange("SheetScopedNamedRange1", WB)
+    Pass = "Column1" = TableR.Cells(1).Value = Pass
+
+    TestTableRange = Pass
+End Function
+
+
+Function Test_CopyTable(WB As Workbook, WB2 As Workbook)
+    Dim Pass As Boolean
+    Pass = True
+
+    Dim LO As ListObject
+    Dim LOEntries As Range
+
+    CopyTable "TableForCopy", WB2.Worksheets(1).Cells(5, 3), , WB, True
+    Set LO = GetLO("TableForCopy", WB2)
+    Set LOEntries = LO.DataBodyRange
+
+    Pass = HasLO("TableForCopy", WB2) = Pass
+    
+    Pass = "General" = LOEntries(1).NumberFormat = Pass
+    Pass = "0.00%" = LOEntries(2).NumberFormat = Pass
+    Pass = "$#,##0.00" = LOEntries(3).NumberFormat = Pass
+    Pass = "General" = LOEntries(4).NumberFormat = Pass
+    Pass = "General" = LOEntries(5).NumberFormat = Pass
+    Pass = "m/d/yyyy" = LOEntries(6).NumberFormat = Pass
+
+    Pass = "=[foo]" = LOEntries(1, 1).Value = Pass
+    Pass = 12 = CInt(LOEntries(1, 2).Value) = Pass
+    Pass = 21 = CInt(LOEntries(2, 1).Value) = Pass
+    Pass = 22 = CInt(LOEntries(2, 2).Value) = Pass
+    Pass = "Hello" = LOEntries(3, 1).Value = Pass
+    Pass = 32 = CInt(LOEntries(3, 2).Value) = Pass
+    Pass = CVErr(xlErrName) = LOEntries(4, 1).Value = Pass
+    Pass = CVErr(xlErrNA) = LOEntries(4, 2).Value = Pass
+    Pass = "=foo" = LOEntries(5, 1).Value = Pass
+    
+    Test_CopyTable = Pass
+End Function
+
+
+Function Test_TableColumnToCollection()
+    Dim Pass As Boolean
+    Pass = True
+
+    Dim col1 As Collection
+    Dim col2 As Collection
+
+    Set col1 = col(Dict("a", 1, "b", 2), Dict("a", 10, "b", 20))
+    Set col2 = TableColumnToCollection(col1, "b")
+
+    Pass = 2 = col2(1) = Pass
+    Pass = 20 = col2(2) = Pass
+
+    Test_TableColumnToCollection = Pass
+End Function
+
+
+Function Test_ResizeLO_1(SelectedTable As ListObject)
+    Test_ResizeLO_1 = 3 = CInt(SelectedTable.ListRows.Count)
+End Function
+
+
+Function Test_ResizeLO_2(SelectedTable As ListObject)
+    Test_ResizeLO_2 = 0 = CInt(SelectedTable.ListRows.Count)
+End Function
+
+
+Function Test_ResizeLO_3(SelectedTable As ListObject)
+    Test_ResizeLO_3 = 2 = CInt(SelectedTable.ListRows.Count)
+End Function
+
+
+Function Test_ResizeLO_4(SelectedTable As ListObject)
+     Test_ResizeLO_4 = 1 = CInt(SelectedTable.ListRows.Count)
+End Function
+
+
+Function Test_GetTableColumnDataRange_fail(SelectedTable As ListObject)
+    Const ExpectedError As Long = 32000
+    On Error GoTo TestFail
+
+    Dim R As Range
+    Set R = GetTableColumnDataRange(SelectedTable, "NonExistingColumn")
+
+    Test_GetTableColumnDataRange_fail = False
+    Exit Function
+TestFail:
+    If Err.Number = ExpectedError Then
+        Test_GetTableColumnDataRange_fail = True
+        Exit Function
+    Else
+        Test_GetTableColumnDataRange_fail = False
+        Exit Function
+    End If
+End Function
+
+
+Function Test_GetTableRowNumberDataRange_fail(SelectedTable As ListObject)
+    Const ExpectedError As Long = 32000
+    On Error GoTo TestFail
+
+    Dim R As Range
+    Set R = GetTableRowNumberDataRange(SelectedTable, 20)
+
+    Test_GetTableRowNumberDataRange_fail = False
+    Exit Function
+    
+TestFail:
+    If Err.Number = ExpectedError Then
+        Test_GetTableRowNumberDataRange_fail = True
+    Else
+        Test_GetTableRowNumberDataRange_fail = False
+    End If
+End Function
+
+
+Function Test_GetTableRowRange1(R As Range)
+    Test_GetTableRowRange1 = "$B$6:$D$6" = R.Address
+End Function
+
+
+Function Test_GetTableRowRange2(R As Range)
+    Test_GetTableRowRange2 = "$G$6:$L$6" = R.Address
+End Function
+
--- tests/testMiscTables.py
+++ tests/testMiscTables.py
@@ -1,103 +1,223 @@
 import unittest
 from xlwings import Book
 from locate import this_dir, prepend_sys_path
-from aa_py_xl import excel
+from aa_py_xl import excel_app
 
 with prepend_sys_path():
-    from util import functions_book, vba_dict
+    from util import functions_book, vba_dict, extra_book
 
 
 class MiscTables(unittest.TestCase):
     def test_1(self) -> None:
-        book: Book
-        with functions_book() as book:
-            func_HasLO = book.macro("MiscTables.HasLO")
-            func_GetLO = book.macro("MiscTables.GetLO")
+        with excel_app(True, True) as app:
+            book: Book
+            with functions_book(app) as book:
+                func_HasLO = book.macro("MiscTables.HasLO")
+                func_GetLO = book.macro("MiscTables.GetLO")
 
-            repo_path = this_dir().parent
-            repo_path = repo_path.joinpath(r"test_data\MiscTables\MiscTablesTests.xlsx")
+                repo_path = this_dir().parent
+                repo_path = repo_path.joinpath(
+                    r"test_data\MiscTables\MiscTablesTests.xlsx"
+                )
 
-            book_temp: Book
-            with excel(
-                path=repo_path,
-                save=False,
-                quiet=True,
-                close_book=True,
-                close_excel=True,
-                must_exist=True,
-                read_only=True,
-            ) as book_temp:
-                with self.subTest("HasLO_and_GetLO"):
-                    self.assertTrue(func_HasLO("Table1", book_temp))  # Correct case
-                    self.assertTrue(
-                        func_HasLO("taBLe1", book_temp)
-                    )  # Should work without correct case
+                book_extra: Book
+                with extra_book(app, repo_path) as book_extra:
+                    with self.subTest("HasLO"):
+                        self.assertTrue(
+                            func_HasLO("Table1", book_extra)
+                        )  # Correct case
+                        self.assertTrue(
+                            func_HasLO("taBLe1", book_extra)
+                        )  # Should work without correct case
 
-                with self.subTest("HasLO_and_GetLO"):
-                    func_GetAllTables = book.macro("MiscTables.GetAllTables")
-                    func_IsValueInCollection = book.macro(
-                        "MiscCollection.IsValueInCollection"
+                    with self.subTest("TestListAllTables"):
+                        func_GetAllTables = book.macro("MiscTables.GetAllTables")
+                        func_IsValueInCollection = book.macro(
+                            "MiscCollection.IsValueInCollection"
+                        )
+
+                        Tables = func_GetAllTables(book_extra)
+
+                        self.assertTrue(func_IsValueInCollection(Tables, "Table1"))
+                        self.assertTrue(func_IsValueInCollection(Tables, "NamedRange1"))
+                        self.assertTrue(
+                            func_IsValueInCollection(Tables, "SheetScopedNamedRange1")
+                        )
+                    with self.subTest("Test_TableColumnToArray"):
+                        func_TableColumnToArray = book.macro(
+                            "MiscTables.TableColumnToArray"
+                        )
+                        func_col = book.macro("MiscCollectionCreate.Col")
+
+                        col = func_col(
+                            vba_dict({"a": 1, "b": 2}), vba_dict({"a": 10, "b": 20})
+                        )
+                        arr = func_TableColumnToArray(col, "b")
+                        self.assertEqual(2, arr[0])
+                        self.assertEqual(20, arr[1])
+
+                    with self.subTest("TableColumnToCollection"):
+                        func_TableColumnToCollection = book.macro(
+                            "MiscTables.TableColumnToCollection"
+                        )
+                        func_col = book.macro("MiscCollectionCreate.Col")
+                        func_Col_to_arr = book.macro("MiscCollection.CollectionToArray")
+
+                        col1 = func_col(
+                            vba_dict({"a": 1, "b": 2}), vba_dict({"a": 10, "b": 20})
+                        )
+
+                        col2 = func_TableColumnToCollection(col1, "b")
+                        arr = func_Col_to_arr(col2)
+
+                        self.assertEqual(2, arr[0])
+                        self.assertEqual(20, arr[1])
+
+                    with self.subTest("GetTableColumnDataRange"):
+                        func_GetTableColumnDataRange = book.macro(
+                            "MiscTables.GetTableColumnDataRange"
+                        )
+                        func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
+                        LO = func_GetLO("table2", book_extra)
+                        arr = func_RangeToArray(
+                            func_GetTableColumnDataRange(LO, "Column2")
+                        )
+                        self.assertEqual(12, arr[0])
+                        self.assertEqual(22, arr[1])
+                        self.assertEqual(32, arr[2])
+
+                    with self.subTest("GetTableRowNumberDataRange"):
+                        func_GetTableRowNumberDataRange = book.macro(
+                            "MiscTables.GetTableRowNumberDataRange"
+                        )
+                        func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
+                        LO = func_GetLO("table2", book_extra)
+                        arr = func_RangeToArray(func_GetTableRowNumberDataRange(LO, 2))
+                        self.assertEqual(21, arr[0])
+                        self.assertEqual(22, arr[1])
+                        self.assertEqual(23, arr[2])
+
+                    with self.subTest("GetTableColumnDataRange_2"):
+
+                        func_GetTableColumnDataRange = book.macro(
+                            "MiscTables.GetTableColumnDataRange"
+                        )
+                        func_ResizeLO = book.macro("MiscTables.ResizeLO")
+                        func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
+                        SelectedTable = func_GetLO("table4", book_extra)
+                        range_obj = func_GetTableColumnDataRange(
+                            SelectedTable, "Column2"
+                        )
+                        func_ResizeLO(SelectedTable, 0)
+                        arr = func_RangeToArray(range_obj)
+                        self.assertEqual(None, arr[0])
+
+                    with self.subTest("TestHasLO_and_GetLO"):
+                        func = book.macro("Test__Helper_MiscTables.TestHasLO_and_GetLO")
+                        self.assertTrue(func(book_extra))
+
+                    with self.subTest("TestTableRange"):
+                        func = book.macro("Test__Helper_MiscTables.TestTableRange")
+                        self.assertTrue(func(book_extra))
+
+                    with self.subTest("Test_CopyTable"):
+                        func = book.macro("Test__Helper_MiscTables.Test_CopyTable")
+                        self.assertTrue(func(book_extra, book))
+
+                    with self.subTest("Test_CopyTable"):
+                        func = book.macro(
+                            "Test__Helper_MiscTables.Test_TableColumnToCollection"
+                        )
+                        self.assertTrue(func())
+
+                    with self.subTest("Test_ResizeLO_1"):
+                        func_ResizeLO = book.macro("MiscTables.ResizeLO")
+                        LO = func_GetLO("Table1", book_extra)
+                        func_ResizeLO(LO, 3)
+                        func = book.macro("Test__Helper_MiscTables.Test_ResizeLO_1")
+                        self.assertTrue(func(LO))
+
+                    with self.subTest("Test_ResizeLO_2"):
+                        func_ResizeLO = book.macro("MiscTables.ResizeLO")
+                        LO = func_GetLO("Table1", book_extra)
+                        func_ResizeLO(LO, 0)
+                        func = book.macro("Test__Helper_MiscTables.Test_ResizeLO_2")
+                        self.assertTrue(func(LO))
+
+                    with self.subTest("Test_ResizeLO_3"):
+                        func_ResizeLO = book.macro("MiscTables.ResizeLO")
+                        LO = func_GetLO("Table1", book_extra)
+                        func_ResizeLO(LO, 5)
+                        func_ResizeLO(LO, 2)
+                        func = book.macro("Test__Helper_MiscTables.Test_ResizeLO_3")
+                        self.assertTrue(func(LO))
+
+                    with self.subTest("Test_ResizeLO_4"):
+                        func_ResizeLO = book.macro("MiscTables.ResizeLO")
+                        LO = func_GetLO("Table1", book_extra)
+                        func_ResizeLO(LO, 0)
+                        func_ResizeLO(LO, 1)
+                        func = book.macro("Test__Helper_MiscTables.Test_ResizeLO_4")
+                        self.assertTrue(func(LO))
+
+                    with self.subTest("Test_GetTableColumnDataRange_fail"):
+                        func = book.macro(
+                            "Test__Helper_MiscTables.Test_GetTableColumnDataRange_fail"
+                        )
+                        LO = func_GetLO("table2", book_extra)
+                        self.assertTrue(func(LO))
+
+                    with self.subTest("Test_GetTableRowNumberDataRange_fail"):
+                        func = book.macro(
+                            "Test__Helper_MiscTables.Test_GetTableRowNumberDataRange_fail"
+                        )
+                        LO = func_GetLO("table2", book_extra)
+                        self.assertTrue(func(LO))
+
+                    repo_path = this_dir().parent
+                    repo_path = repo_path.joinpath(
+                        r"test_data\MiscTableToDicts\MiscTableToDicts.xlsx"
                     )
 
-                    Tables = func_GetAllTables(book_temp)
+                    book_temp: Book
+                    with extra_book(app, repo_path) as book_temp:
+                        with self.subTest("Test_GetTableRowRange1"):
+                            func_col = book.macro("MiscCollectionCreate.Col")
 
-                    self.assertTrue(func_IsValueInCollection(Tables, "Table1"))
-                    self.assertTrue(func_IsValueInCollection(Tables, "NamedRange1"))
-                    self.assertTrue(
-                        func_IsValueInCollection(Tables, "SheetScopedNamedRange1")
-                    )
+                            func = book.macro(
+                                "Test__Helper_MiscTables.Test_GetTableRowRange1"
+                            )
 
-                with self.subTest("TableColumnToCollection"):
-                    func_TableColumnToCollection = book.macro(
-                        "MiscTables.TableColumnToCollection"
-                    )
-                    func_col = book.macro("MiscCollectionCreate.Col")
-                    func_Col_to_arr = book.macro("MiscCollection.CollectionToArray")
+                            func_GetTableRowRange = book.macro(
+                                "MiscTables.GetTableRowRange"
+                            )
+                            R = func_GetTableRowRange(
+                                "ListObject1",
+                                func_col("a", "b"),
+                                func_col(4, 5),
+                                book_temp,
+                            )
 
-                    col1 = func_col(
-                        vba_dict({"a": 1, "b": 2}), vba_dict({"a": 10, "b": 20})
-                    )
+                            self.assertTrue(func(R))
 
-                    col2 = func_TableColumnToCollection(col1, "b")
-                    arr = func_Col_to_arr(col2)
+                        with self.subTest("Test_GetTableRowRange2"):
+                            func_col = book.macro("MiscCollectionCreate.Col")
 
-                    self.assertEqual(2, arr[0])
-                    self.assertEqual(20, arr[1])
+                            func = book.macro(
+                                "Test__Helper_MiscTables.Test_GetTableRowRange2"
+                            )
 
-                with self.subTest("GetTableColumnDataRange"):
-                    func_GetTableColumnDataRange = book.macro(
-                        "MiscTables.GetTableColumnDataRange"
-                    )
-                    func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
-                    LO = func_GetLO("table2", book_temp)
-                    arr = func_RangeToArray(func_GetTableColumnDataRange(LO, "Column2"))
-                    self.assertEqual(12, arr[0])
-                    self.assertEqual(22, arr[1])
-                    self.assertEqual(32, arr[2])
+                            func_GetTableRowRange = book.macro(
+                                "MiscTables.GetTableRowRange"
+                            )
+                            R = func_GetTableRowRange(
+                                "NamedRange1",
+                                func_col("a", "b"),
+                                func_col(4, 5),
+                                book_temp,
+                            )
 
-                with self.subTest("GetTableRowNumberDataRange"):
-                    func_GetTableRowNumberDataRange = book.macro(
-                        "MiscTables.GetTableRowNumberDataRange"
-                    )
-                    func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
-                    LO = func_GetLO("table2", book_temp)
-                    arr = func_RangeToArray(func_GetTableRowNumberDataRange(LO, 2))
-                    self.assertEqual(21, arr[0])
-                    self.assertEqual(22, arr[1])
-                    self.assertEqual(23, arr[2])
-
-                with self.subTest("GetTableColumnDataRange_2"):
-
-                    func_GetTableColumnDataRange = book.macro(
-                        "MiscTables.GetTableColumnDataRange"
-                    )
-                    func_ResizeLO = book.macro("MiscTables.ResizeLO")
-                    func_RangeToArray = book.macro("MiscRangeToArray.RangeToArray")
-                    SelectedTable = func_GetLO("table4", book_temp)
-                    range_obj = func_GetTableColumnDataRange(SelectedTable, "Column2")
-                    func_ResizeLO(SelectedTable, 0)
-                    arr = func_RangeToArray(range_obj)
-                    self.assertEqual(None, arr[0])
+                            self.assertTrue(func(R))
 
 
 if __name__ == "__main__":
