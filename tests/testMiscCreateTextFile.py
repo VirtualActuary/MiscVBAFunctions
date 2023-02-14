aad17b1769ae3611acfa12d84036b5c8774e3cc5
Bart-Jan Hulsman <91005483+BartJan142@users.noreply.github.com>
2023-02-14 11:42:00 +0200

    Added `ReadTextFile` test function

--- tests/testMiscCreateTextFile.py
+++ tests/testMiscCreateTextFile.py
@@ -6,11 +6,11 @@ with prepend_sys_path():
     from util import functions_book
 
 
-class MiscCreateTextFile(unittest.TestCase):
+class MiscTextFile(unittest.TestCase):
     def test_1(self) -> None:
         with functions_book() as book:
             with self.subTest("CreateTextFile"):
-                func_CreateTextFile = book.macro("MiscCreateTextFile.CreateTextFile")
+                func_CreateTextFile = book.macro("MiscTextFile.CreateTextFile")
                 func_EvalPath = book.macro("MiscPath.EvalPath")
                 inputText = "my test text."
                 FilePath = func_EvalPath(r".\test_data\MiscCreateTextFile\test.txt")
@@ -20,6 +20,15 @@ class MiscCreateTextFile(unittest.TestCase):
                     lines = [line.rstrip() for line in file]
 
                 self.assertEqual("my test text.", lines[0])
+
+            with self.subTest("ReadTextFile"):
+                func_CreateTextFile = book.macro("MiscTextFile.CreateTextFile")
+                func_ReadTextFile = book.macro("MiscTextFile.ReadTextFile")
+                func_EvalPath = book.macro("MiscPath.EvalPath")
+                inputText = "my test text."
+                FilePath = func_EvalPath(r".\test_data\MiscCreateTextFile\test.txt")
+                func_CreateTextFile(inputText, FilePath)
+                self.assertEqual("my test text.", func_ReadTextFile(FilePath).strip())
 
 
 if __name__ == "__main__":
