f2c07ba4f6bea4af9d51424138e9a2c715b3536c
Bart-Jan Hulsman <91005483+BartJan142@users.noreply.github.com>
2023-02-15 15:02:16 +0200

    Moved `Zip` tests to Python

--- tests/testMiscCollectionCreate.py
+++ tests/testMiscCollectionCreate.py
@@ -19,8 +19,21 @@ class MiscCollectionCreate(unittest.TestCase):
 
             with self.subTest("zip"):
                 func_col = book.macro("MiscCollectionCreate.Col")
-                func = book.macro("Test__Helper_MiscCollCreate.Test_zip")
-                self.assertTrue(func(func_col(1, 2, 3), func_col(4, 5, 6, 7)))
+                func_zip = book.macro("MiscCollectionCreate.Zip")
+                func_Col_to_arr = book.macro("MiscCollection.CollectionToArray")
+
+                c1 = func_zip(func_col(1, 2, 3), func_col(4, 5, 6, 7))
+                arr = func_Col_to_arr(c1)
+                self.assertEqual(((1, 4), (2, 5), (3, 6)), arr)
+
+            with self.subTest("zip2"):
+                func_col = book.macro("MiscCollectionCreate.Col")
+                func_zip = book.macro("MiscCollectionCreate.Zip")
+                func_Col_to_arr = book.macro("MiscCollection.CollectionToArray")
+
+                c1 = func_zip(func_col(1, 2, 3), func_col(4, 5, 6, 7), func_col(10, 12))
+                arr = func_Col_to_arr(c1)
+                self.assertEqual(((1, 4, 10), (2, 5, 12)), arr)
 
 
 if __name__ == "__main__":
