4dca420ebbe6539442232e32ebc0b9302bf84f7f
dolf <rudolfbyker@gmail.com>
2024-01-23 13:47:15 +0200

    Add failing test for `ArrayToRange`: It must preserve data types. No auto-conversion is desired.

--- tests/MiscArray/testArrayToRange.py
+++ tests/MiscArray/testArrayToRange.py
@@ -45,3 +45,37 @@ class TestArrayToRange(TestCaseWithFunctionBook):
         arr = ["col1", "col2", "col3"]
         range_obj = self.book.sheets.active.range("B4")
         self.assertTrue(self.array_to_range_fail(arr, range_obj))
+
+    def test_preserve_all_data_types(self) -> None:
+        """
+        Data types must be preserved:
+        - Strings should stay strings, even if it looks like it might contain something else.
+        - Numbers should stay numbers. Excel sometimes wants to convert them to dates.
+
+        If data needs to be converted from one type to another, that belongs on a business logic level, and not
+        in a low-level function like this one.
+
+        See https://github.com/AutoActuary/autory/issues/1150
+        """
+        data = [
+            ["TRUE", "FALSE"],  # Strings that look like booleans.
+            ["#VALUE!", "#N/A"],  # Strings that look like errors.
+            ["1", "2"],  # Strings that look like integers.
+            ["1.2", "2.3"],  # Strings that look like floats.
+            ["1.2.3", "2.3.4"],  # Strings that look like version numbers.
+            ["2022/11/02", "20231020T123456"],  # Strings that look like dates / times.
+            [1.2, 2.3],  # These are floats, but Excel sometimes sees them as dates.
+            [1, 2],  # These are integers, but Excel sometimes sees them as dates.
+            [True, False],  # Booleans.
+        ]
+
+        self.array_to_range(
+            data,
+            self.book.sheets["Sheet1"].range("A1"),
+            False,  # Escape formulas.
+            False,  # No header.
+        )
+        self.assertEqual(
+            data,
+            self.book.sheets[0].range((1, 1), (len(data), len(data[0]))).value,
+        )
